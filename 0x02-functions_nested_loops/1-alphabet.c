efd7319ec0a7f9b864ff7e0bd4fd1f61a3e44bb1
Emmanuella111 <emmanuellaogbodu@gmail.com>
2022-07-05 14:56:41 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * print_alphabet - Print the lowercase alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z';
+	{
+	     _putchar 'letter';
+	}
+	_putchar '\n';
+}
