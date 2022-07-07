d07ec0f4471488a6e977bd874eb897a00233fd31
Emmanuella111 <emmanuellaogbodu@gmail.com>
2022-07-07 16:23:47 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 1-isdigit.c: A function that checks for digits.
+ * @d: digit from 0 to 9
+ */
+
+
+int isdigit.c(int d)
+
+{
+	int digit = '0';
+
+	int isdigit.c = 0;
+
+
+	for (; digit <= '9'; digit +++)
+	{
+		if (d == digit)
+		{
+			isdigit.c = 1;
+			break;
+		}
+		return isdigit.c
+	}
+}
