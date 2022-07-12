c627525fabdc95acae557b29be73e6c977dcb4b4
Emmanuella111 <emmanuellaogbodu@gmail.com>
2022-07-12 13:33:50 -0700

    task 0 and advanced

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,54 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
-main() {
-    
-    int a = 20
-    int b = 10
-    int c;
-    
-    c = a + b
+/**
+*reset_to_98 - set value of int to 98
+*@n: address of value to change
+*Description: resets *n to 98
+*Return: nothing
+*/
 
-
-No. 3 
-}
-if ( 5 < 10 )
-    printf( "Five is now less than ten, that's a big surprise" );
-    
-    
-
-
-if ( TRUE ) {
-
-    /* Execute these statements if TRUE */
-
-}
-
-else {
-
-    /* Execute these statements if FALSE */
-
-}
-
-
-No. 4
-
-#include <stdio.h>    
- 
-int main()                            /* Most important part of the program!  */
+void reset_to_98(int *n)
 {
-    int age;                          /* Need a variable... */
-   
-    printf( "Please enter your age: " );  /* Asks for age */
-    scanf( "%d", &age );                 /* The input is put in age */
-    if ( age < 100 ) {                  /* If the age is less than 100 */
-        printf ("You are pretty young!\n" ); /* Just to show you it works... */
-    }
-    else if ( age == 100 ) {            /* I use else just to show an example */ 
-        printf( "You are old\n" );           
-    }
-    else {
-        printf( "You are really old\n" );     /* Executed if no other statement is */
-    }
-  return 0;
- 
+	*n = 98;
 }
